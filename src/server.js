3e1778fe378e51b3fcf824e2f022f23ed69ee9a1
Triaaz <alimtiaaz23@gmail.com>
2026-06-02 10:24:53 +0100

    connect mongodb atlas

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
+// MongoDB Atlas connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Backend API running");
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
