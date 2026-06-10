138f47e3ddf55ad1d629626e0b43c11dd5ac1d3f
Triaaz <alimtiaaz23@gmail.com>
2026-06-10 07:25:57 +0100

    Fix database environment variable mismatch

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async() =>{
     try {
-        await mongoose.connect(process.env.LIVE_URL)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('Database connected successfully')
     } catch (error) {
         console.error('Database connection failed',error.message)
